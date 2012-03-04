f86a447103dfdd29f583bdc76968576c2f87dfe0
Konstantin Shabanov <etehtsea@gmail.com>
2012-03-05 02:33:15 +0400

    Remove trailing whitespace

--- test/helper.rb
+++ test/helper.rb
@@ -36,7 +36,7 @@ module TestHelper
       UnixUtils.du(infile_or_srcdir).must_equal size
     end
   end
-  
+
   def assert_unpack_dir(method_id, infile)
     destdir = UnixUtils.send method_id, infile
     File.directory?(destdir).must_equal true
@@ -44,7 +44,7 @@ module TestHelper
     File.dirname(destdir).start_with?(Dir.tmpdir).must_equal true
     safe_delete destdir
   end
-  
+
   def assert_unpack_file(method_id, infile)
     outfile = UnixUtils.send method_id, infile
     File.file?(outfile).must_equal true
@@ -52,7 +52,7 @@ module TestHelper
     File.dirname(outfile).start_with?(Dir.tmpdir).must_equal true
     safe_delete outfile
   end
-    
+
   def assert_pack(method_id, infile)
     outfile = UnixUtils.send method_id, infile
     File.file?(outfile).must_equal true
@@ -60,7 +60,7 @@ module TestHelper
     File.dirname(outfile).start_with?(Dir.tmpdir).must_equal true
     safe_delete outfile
   end
-  
+
   def safe_delete(path)
     path = File.expand_path path
     raise "Refusing to rm -rf #{path} because it's not in #{Dir.tmpdir}" unless File.dirname(path).start_with?(Dir.tmpdir)
