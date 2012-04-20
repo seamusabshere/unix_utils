5245000692d80c88f72e3758118363b0705cbed4
Seamus Abshere <seamus@abshere.net>
2012-04-19 19:03:52 -0500

    not necessary because Bundler is in charge

--- test/helper.rb
+++ test/helper.rb
@@ -7,8 +7,6 @@ require 'minitest/reporters'
 MiniTest::Unit.runner = MiniTest::SuiteRunner.new
 MiniTest::Unit.runner.reporters << MiniTest::Reporters::SpecReporter.new
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'unix_utils'
 
 require 'stringio'
